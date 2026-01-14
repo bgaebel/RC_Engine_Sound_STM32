68948f97528adf59404f76b98bcd92b304722055
bgaebel <41983904+bgaebel@users.noreply.github.com>
2026-01-14 21:57:39 +0100

    Use vehicle sound arrays directly

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -34,7 +34,6 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "Vehicle.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
